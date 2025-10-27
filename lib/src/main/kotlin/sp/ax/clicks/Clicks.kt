29d898ea2061ea9bd745ab793910219cb80c0415
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-27 21:48:28 +0300

    clicks

--- lib/src/main/kotlin/sp/ax/clicks/Clicks.kt
+++ lib/src/main/kotlin/sp/ax/clicks/Clicks.kt
@@ -1,6 +1,10 @@
 package sp.ax.clicks
 
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.gestures.PressGestureScope
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.runtime.Composable
@@ -8,7 +12,76 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.debugInspectorInfo
+
+fun Modifier.clicks(
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource,
+    indication: Indication,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+): Modifier {
+    return composed(
+        inspectorInfo = debugInspectorInfo {
+            name = "clicks"
+            properties["enabled"] = enabled
+            properties["onClick"] = onClick
+            properties["onLongClick"] = onLongClick
+            properties["indication"] = indication
+            properties["interactionSource"] = interactionSource
+        },
+        factory = {
+            val onClickState = rememberUpdatedState(onClick)
+            val onLongClickState = rememberUpdatedState(onLongClick)
+            val lastPressState = getLastPressState(
+                enabled = enabled,
+                interactionSource = interactionSource,
+            )
+            Modifier
+                .indication(interactionSource = interactionSource, indication = indication)
+                .pointerInput(interactionSource, enabled) {
+                    detectTapGestures(
+                        onPress = { offset ->
+                            if (enabled) {
+                                onPress(
+                                    offset = offset,
+                                    lastPressState = lastPressState,
+                                    interactionSource = interactionSource,
+                                )
+                            }
+                        },
+                        onTap = {
+                            if (enabled) onClickState.value()
+                        },
+                        onLongPress = {
+                            if (enabled) onLongClickState.value()
+                        },
+                    )
+                }
+        },
+    )
+}
+
+fun Modifier.clicks(
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+): Modifier {
+    return composed {
+        clicks(
+            enabled = enabled,
+            interactionSource = remember { MutableInteractionSource() },
+            indication = LocalIndication.current,
+            onClick = onClick,
+            onLongClick = onLongClick,
+        )
+    }
+}
 
 @Composable
 internal fun getLastPressState(
