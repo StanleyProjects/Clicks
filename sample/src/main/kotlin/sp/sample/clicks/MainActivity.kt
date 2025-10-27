832e08d3dbbe790ba6e24978f9bab8b149e06ece
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-27 17:40:31 +0300

    indication

--- sample/src/main/kotlin/sp/sample/clicks/MainActivity.kt
+++ sample/src/main/kotlin/sp/sample/clicks/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.IndicationNodeFactory
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,12 +17,51 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.node.DelegatableNode
+import androidx.compose.ui.node.DrawModifierNode
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import sp.ax.clicks.onClick
 
 internal class MainActivity : ComponentActivity() {
+    private class FooNode(private val interactionSource: InteractionSource) : Modifier.Node(), DrawModifierNode {
+        override fun onAttach() {
+            coroutineScope.launch {
+                interactionSource.interactions.collect { interaction ->
+                    when (interaction) {
+                        is PressInteraction.Press -> {
+                            println("Press")
+                        }
+                        is PressInteraction.Release -> {
+                            println("Release")
+                        }
+                        is PressInteraction.Cancel -> {
+                            println("Cancel")
+                        }
+                        else -> {
+                            println("Unknown interaction: ${interaction::class.java.name}")
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun ContentDrawScope.draw() {
+            drawContent()
+        }
+    }
+
+    private data object FooIndicationNodeFactory : IndicationNodeFactory {
+        override fun create(interactionSource: InteractionSource): DelegatableNode {
+            return FooNode(interactionSource = interactionSource)
+        }
+    }
+
     private fun showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(this, message, duration).show()
     }
@@ -37,6 +80,31 @@ internal class MainActivity : ComponentActivity() {
                             }
                             .wrapContentSize(),
                         text = "foo",
+                    )
+                    BasicText(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(48.dp)
+                            .onClick {
+                                showToast("bar")
+                            }
+                            .wrapContentSize(),
+                        text = "bar",
+                    )
+                    BasicText(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(48.dp)
+                            .onClick(
+                                enabled = true,
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = FooIndicationNodeFactory,
+                                block = {
+                                    showToast("baz")
+                                },
+                            )
+                            .wrapContentSize(),
+                        text = "baz",
                     )
                 }
             }
